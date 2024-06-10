bb52daa4af37bff61091c52be7e58afd54ed8ee2
Артем Конотопчик <61752968+MiMiMiEnot@users.noreply.github.com>
2024-06-10 15:23:28 +0300

    Update Langs.java

--- WellChat/src/main/java/me/enot/wellchat/configurations/language/Langs.java
+++ WellChat/src/main/java/me/enot/wellchat/configurations/language/Langs.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 public enum Langs {
 
-    advertisement__broadcast_to_moderators("&cCC &f{name} &8➯ &f{message}"),
-    command__wellauth__usage("&fИспользуй так: &3/wellauth reload&f для перезагрузки конфигурационных файлов"),
-    command__wellauth__errors__no_perms("&cУ тебя нет прав для использования этой команды!"),
-    command__wellauth__reload_succesful("&aКонфигурационные файлы плагина успешно перезагружено");
+    ;
 
     List<String> list;
     Langs(String... def){
